f6ff1d8162379c20f4fed0ed94a2d65a8655ce15
Abdus Sobhan <abdus.sobhan.vu@gmail.com>
2025-08-07 12:27:02 +0600

    Initial commit for Golang folder

---
+++ Go With Habib/8. variables_datatype/var2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 69
+	var b = 2.5652
+	name := "Abdus Sobhan"
+	bo := true
+	bo = false
+
+	const pi = 3.14159265358979323846
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(name)
+	fmt.Println(bo)
+	fmt.Print(pi)
+}
